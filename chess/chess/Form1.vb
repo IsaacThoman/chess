aba7494826b3504c55348558a3a556e0c19ac338
isaacthoman <isaacthoman@outlook.com>
2020-12-26 18:53:57 -0500

    piece placement and like the most basic pen stuff

---
+++ chess/chess/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+
+    Public boardSquare(8, 8) As Integer
+
+    Function setBoard()
+        'white rooks
+        boardSquare(1, 1) = 4
+        boardSquare(8, 1) = 4
+        'black rooks
+        boardSquare(1, 8) = 10
+        boardSquare(8, 8) = 10
+        'white knights
+        boardSquare(2, 1) = 3
+        boardSquare(7, 1) = 3
+        'black knights
+        boardSquare(2, 8) = 9
+        boardSquare(7, 8) = 9
+        'white bishops
+        boardSquare(3, 1) = 2
+        boardSquare(6, 1) = 2
+        'black bishops
+        boardSquare(3, 8) = 9
+        boardSquare(6, 8) = 9
+        'kings
+        boardSquare(4, 1) = 6
+        boardSquare(4, 8) = 12
+        'queens
+        boardSquare(5, 1) = 6
+        boardSquare(5, 8) = 12
+
+        'this should fill in pawns
+        Dim filler As Integer
+        For filler = 1 To 8
+            boardSquare(filler, 2) = 1
+            boardSquare(filler, 7) = 7
+        Next
+
+
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ penstuff2/penstuff2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim surface As Graphics = CreateGraphics()
+        Dim Brush1 As Brush
+        Dim solidb As SolidBrush = New SolidBrush(Color.DarkGreen)
+        Dim rect1 As Rectangle = New Rectangle(0, 0, 50, 50)
+
+        surface.FillRectangle(solidb, rect1)
+
+    End Sub
+End Class
